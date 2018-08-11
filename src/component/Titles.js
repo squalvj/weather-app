96400d56a9a36499e42ff4c4ec87dc8ab431d019
Aditya Wiraha Hadi Saputra <squalvj@Adityas-MacBook-Pro.local>
2018-08-11 21:05:56 +0800

    init

---
+++ src/component/Titles.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Titles = props => (
+
+    <div> 
+        <h1 className="title-container__title">Weather Finder </h1>
+        <p className="title-container__subtitle">Find out temperature, condition and more...</p>
+    </div>
+
+)
+
+
+// class Titles extends React.Component {
+
+//     render() {
+
+//         return(
+
+//             <div> 
+//                 <h1>Weather Finder </h1>
+//                 <p>Find out temperature, condition and more...</p>
+//             </div>
+
+//         )
+
+//     }
+    
+// }
+
+export default Titles;
